43727d28b6ee5bf3b70e7885f6111ca064451397
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2020-05-20 14:00:47 +0400

    Add files via upload

---
+++ ClassLibrary/ClassLibrary/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Notification
+    {
+        internal string Text="Salam P116";
+        protected internal string Title;
+        private protected int Count;
+        public string getMessage()
+        {
+            return Text;
+        }
+    }
+
+    internal class SmsNotification: Notification
+    {
+        public SmsNotification()
+        {
+            Title = "Hello";
+            Count = 6;
+        }
+    }
+
+    internal class Library
+    {
+        public Library()
+        {
+            Notification n = new Notification();
+            n.Text = "Your have new notify";
+            n.Title = "Hi";
+            
+        }
+    }
+}
